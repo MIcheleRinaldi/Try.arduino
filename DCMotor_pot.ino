f5f8a76ed80d917d47df61ecda6584be85bad888
Michele Rinaldi <michelerinaldi.1991@gmail.com>
2014-01-23 09:36:24 +0100

    Update DCMotor_pot.ino
    Codice più pulito per il motorDC con potenziometro!

--- DCMotor_pot.ino
+++ DCMotor_pot.ino
@@ -1,10 +1,14 @@
 const int potPin = 0;                    //Assegno il potenziometro al pin 0;
 const int transistorPin = 11;            //Assegno il transistor al pin 11;
-int potValue = 0;                       //Variabile potenza che inizialmente è di 0;
+int potValue;                            //Variabile potenza che inizialmente è di 0;
+int potValueMapped;
+
 void setup() {
-pinMode(transistorPin, OUTPUT);          //Decido che il mio output finisce al pin 11;
+  pinMode(transistorPin, OUTPUT);         //Decido che il mio output finisce al pin 11;
 }
-void loop() {                            //Creo una funzione standard di arduino che mi consente di;
-potValue = analogRead(potPin) / 4;       //Legge il valore del pin analogico selezionato;
-analogWrite(transistorPin, potValue);    //Scrive il valore analogico del pin 11 in questo caso e vi aggiunge il valore di potValue;
+
+void loop() {                             //Creo una funzione standard di arduino che mi consente di;
+  potValue = analogRead(potPin);          //Legge il valore del pin analogico selezionato;
+  potValueMapped = map(potValue, 0, 1024, 0 , 255  );
+  analogWrite(transistorPin, potValueMapped);    //Scrive il valore analogico del pin 11 in questo caso e vi aggiunge il valore di potValue;
 }
